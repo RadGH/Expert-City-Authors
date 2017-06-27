5dea260d6095d43d44d85529d89751f7e43f3c90
Radley <radleygh@gmail.com>
2017-06-27 16:11:37 -0700

    Fixed the install message appearing on plugins page because it wasn't looking at the options table

--- includes/options.php
+++ includes/options.php
@@ -16,7 +16,7 @@ function eca_after_install_setup_notice() {
 	if ( !is_admin() ) return;
 	if ( get_option('eca_installed') ) return; // This option is autoloaded and won't slow down every admin menu request
 
-	if ( get_field('eca_enable_author_submissions') != null ) { // User has entered a value, remember that to not ask again
+	if ( get_field('eca_enable_author_submissions', 'options') != null ) { // User has entered a value, remember that to not ask again
 		update_option('eca_installed', '1', true);
 		return;
 	}
